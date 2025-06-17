9f2cef2f0642e69c3863ccf0416bfdaedf925da7
Alvin390 <alvinkariuki07@gmail.com>
2025-06-17 11:46:51 +0300

    Concepts Javascripts

---
+++ src/intro.js
@@ -0,0 +1,6 @@
+
+
+console.log (43*5)
+
+// document.write("Welcome to WebStorm!<br>")
+
---
+++ src/variables.js
@@ -0,0 +1,17 @@
+// variables are declared using `const` or `let` keywords, depending on whether they are reassigned later.
+//   const is immutable while let is mutable
+// var is the old method of declaring variables
+// TIP: In modern JavaScript, it's recommended to use `let` and `const` for variable declarations instead of `var`.
+
+
+
+var num1= 10; // old way of declaring variable
+let num2 = 20; // new way of declaring variables, mutable
+const num3 = 30; // new way of declaring variables, immutable
+
+var name = "Alvin"; // old way of declaring variable string
+
+console.log (name);
+
+let friend= "Kevin"
+console.log (friend);
